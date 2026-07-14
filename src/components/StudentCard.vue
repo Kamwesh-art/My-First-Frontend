51d5e60e0ca38f7482d7200cab13e032e4ad1b88
Kamwesh-art <9274lucy@gmail.com>
2026-07-14 11:45:00 +0300

    intoduction of a new component

---
+++ src/components/StudentCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h2>Student Profile</h2>
+        <p>Name: Darling </p>
+        <p>Course: BBIT</p>
+        <p>Age: 20</p>
+    </div>
+
+</template>
+<script setup lang="ts">
+
+</script>
